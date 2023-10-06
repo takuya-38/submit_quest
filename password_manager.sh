c7b8969abfb0f0ed81efee336f282a4073ad69e0
takuya-38 <takuya.kogoma.38@gmail.com>
2023-10-06 14:16:04 +0000

    ステップ１作成完了

--- password_manager.sh
+++ password_manager.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 #パスワードマネージャーのプログラム
+
+echo "パスワードマネージャーへようこそ！"
+
+#ユーザーからサービス名、ユーザー名、パスワードを受け取る
+read -p 'サービス名を入力してください:' service_name
+read -p 'ユーザー名を入力してください:' user_name
+read -p 'パスワードを入力してください:' password
+
+#入力をパスワードファイルへ追加
+echo "$service_name:$user_name:$password" >> passwords.txt
+
+#プログラム終了
+echo "Thank you!"
